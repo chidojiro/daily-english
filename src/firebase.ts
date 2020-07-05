cb599f2f9e062e0ccb5dce28f36eb2f079d6d7e9
ngoc.maituan <ngoc@theapipractice.com>
2020-07-05 13:22:41 +0700

    fix: change firebase hosting domain

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,14 @@ import * as firebase from 'firebase/app';
 const isDev = process.env.NODE_ENV === 'development';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyANAgn1PwbkgUkcOk81QrAVp0J3-nsDMaM',
-  authDomain: 'vocab-cf44c.firebaseapp.com',
-  databaseURL: 'https://vocab-cf44c.firebaseio.com',
-  projectId: 'vocab-cf44c',
-  storageBucket: 'vocab-cf44c.appspot.com',
-  messagingSenderId: '161628363016',
-  appId: '1:161628363016:web:d90f3f345535b51af934bd',
-  measurementId: 'G-V2N0F9ET4P',
+  apiKey: 'AIzaSyCOZRs7UzfEhcewnvmQnAGVPM3vW0c2fZU',
+  authDomain: 'daivoca.firebaseapp.com',
+  databaseURL: 'https://daivoca.firebaseio.com',
+  projectId: 'daivoca',
+  storageBucket: 'daivoca.appspot.com',
+  messagingSenderId: '205114645226',
+  appId: '1:205114645226:web:94cfca76e9c05c2bffd593',
+  measurementId: 'G-PZ3P2SL14E',
 };
 
 const devFirebaseConfig = {
